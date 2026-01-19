823904c8bad656bf9439689ecfa1a0abca350afe
Darryl Gamroth <dgamroth@rubus.ca>
2026-01-19 12:44:28 -0800

    Add Phase 1 optics tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,23 @@ using Test
 using AdaptiveOptics
 
 @test AdaptiveOptics.PROJECT_STATUS == :in_development
+
+@testset "Telescope and PSF" begin
+    tel = Telescope(resolution=32, diameter=8.0, sampling_time=1e-3, central_obstruction=0.2)
+    src = Source(band=:I, magnitude=0.0)
+    psf = compute_psf!(tel, src; zero_padding=2)
+    @test size(psf) == (64, 64)
+    @test maximum(psf) > 0
+    @test isfinite(sum(psf))
+end
+
+@testset "Zernike basis" begin
+    tel = Telescope(resolution=32, diameter=8.0, sampling_time=1e-3, central_obstruction=0.0)
+    zb = ZernikeBasis(tel, 5)
+    compute_zernike!(zb, tel)
+    @test size(zb.modes) == (32, 32, 5)
+    @test noll_to_nm(1) == (0, 0)
+    @test noll_to_nm(2) == (1, -1)
+    @test noll_to_nm(3) == (1, 1)
+    @test noll_to_nm(4) == (2, -2)
+end
