9c26d3280310671d90486a0ddd8b83b329bf5228
Darryl Gamroth <dgamroth@rubus.ca>
2026-03-17 14:47:58 -0700

    Fix CUDA HIL wrapper world age

--- scripts/gpu_hil_contract.jl
+++ scripts/gpu_hil_contract.jl
@@ -1,10 +1,16 @@
 using AdaptiveOpticsSim
 
-function run_gpu_hil_smoke(::Type{B}) where {B<:GPUBackendTag}
+if !isdefined(@__MODULE__, :run_gpu_smoke_matrix)
     include(joinpath(@__DIR__, "gpu_smoke_contract.jl"))
+end
+
+if !isdefined(@__MODULE__, :run_gpu_builder_smoke)
     include(joinpath(@__DIR__, "gpu_builder_contract.jl"))
-    run_gpu_smoke_matrix(B)
-    run_gpu_builder_smoke(B)
+end
+
+function run_gpu_hil_smoke(::Type{B}) where {B<:GPUBackendTag}
+    Base.invokelatest(run_gpu_smoke_matrix, B)
+    Base.invokelatest(run_gpu_builder_smoke, B)
     println("gpu_hil_smoke complete")
     return nothing
 end
