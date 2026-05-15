54f80a87521a2b6435451633107bc458975f7cbc
Darryl Gamroth <dgamroth@rubus.ca>
2026-05-15 01:13:52 -0700

    Experiment with StaticArrays misregistration transform

--- src/optics/misregistration.jl
+++ src/optics/misregistration.jl
@@ -5,18 +5,42 @@ struct Misregistration{T<:AbstractFloat}
     anamorphosis_angle_rad::T
     tangential_scaling::T
     radial_scaling::T
+    transform::SMatrix{2,2,T,4}
 end
 
 function Misregistration(; shift_x::Real=0.0, shift_y::Real=0.0, rotation_deg::Real=0.0,
     anamorphosis_angle::Real=0.0, tangential_scaling::Real=1.0, radial_scaling::Real=1.0, T::Type{<:AbstractFloat}=Float64)
+    θ = T(deg2rad(anamorphosis_angle))
+    sθ, cθ = sincos(θ)
+    φ = T(deg2rad(rotation_deg))
+    sφ, cφ = sincos(φ)
+    tan_scale = T(tangential_scaling)
+    rad_scale = T(radial_scaling)
+    anamorph = @SMatrix [
+        cθ sθ
+        -sθ cθ
+    ]
+    inv_anamorph = @SMatrix [
+        cθ -sθ
+        sθ cθ
+    ]
+    scale = @SMatrix [
+        tan_scale zero(T)
+        zero(T) rad_scale
+    ]
+    rotation = @SMatrix [
+        cφ -sφ
+        sφ cφ
+    ]
 
     return Misregistration{T}(
         T(shift_x),
         T(shift_y),
-        T(deg2rad(rotation_deg)),
-        T(deg2rad(anamorphosis_angle)),
-        T(tangential_scaling),
-        T(radial_scaling),
+        φ,
+        θ,
+        tan_scale,
+        rad_scale,
+        rotation * inv_anamorph * scale * anamorph,
     )
 end
 
@@ -43,21 +67,6 @@ end
 end
 
 function apply_misregistration(mis::Misregistration, x::Real, y::Real)
-    θ = mis.anamorphosis_angle_rad
-    sθ, cθ = sincos(θ)
-    x1 = cθ * x + sθ * y
-    y1 = -sθ * x + cθ * y
-
-    x1 *= mis.tangential_scaling
-    y1 *= mis.radial_scaling
-
-    x2 = cθ * x1 - sθ * y1
-    y2 = sθ * x1 + cθ * y1
-
-    φ = mis.rotation_rad
-    sφ, cφ = sincos(φ)
-    xr = cφ * x2 - sφ * y2
-    yr = sφ * x2 + cφ * y2
-
-    return xr - mis.shift_x, yr - mis.shift_y
+    v = mis.transform * SVector(x, y)
+    return v[1] - mis.shift_x, v[2] - mis.shift_y
 end
