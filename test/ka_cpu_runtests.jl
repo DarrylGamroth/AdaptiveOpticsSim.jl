9708329706f3ff9c2f8113e9a3dc47cf8148131a
Darryl Gamroth <dgamroth@rubus.ca>
2026-07-29 13:45:53 -0700

    test: load Optics in KA coverage runner

--- test/ka_cpu_runtests.jl
+++ test/ka_cpu_runtests.jl
@@ -1,5 +1,6 @@
 using Test
 using AdaptiveOpticsSim
+using AdaptiveOpticsSim.Optics
 using AdaptiveOpticsSim.Backends
 using AdaptiveOpticsSim: Plant
 using AdaptiveOpticsSim.Plant
@@ -27,6 +28,14 @@ for name in names(Backends; all=true)
     if Base.isidentifier(s) && !startswith(s, "#") &&
             !isdefined(@__MODULE__, name)
         @eval const $(name) = getfield(Backends, $(QuoteNode(name)))
+    end
+end
+
+for name in names(Optics; all=true)
+    s = String(name)
+    if Base.isidentifier(s) && !startswith(s, "#") &&
+            !isdefined(@__MODULE__, name)
+        @eval const $(name) = getfield(Optics, $(QuoteNode(name)))
     end
 end
 
